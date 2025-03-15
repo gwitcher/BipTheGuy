ade21742cfa0ebee83fe7d912dddbb609aaee863
Gabe Witcher <gabewitcher@gmail.com>
2025-03-15 12:00:15 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Gabe Witcher on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
